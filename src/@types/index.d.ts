12b46970d7d7b843ca5c2b7884e1b4180514e98c
André <andre.murillo.correa@gmail.com>
2020-08-09 15:16:07 -0300

    initial setup

---
+++ src/@types/index.d.ts
@@ -0,0 +1,1 @@
+declare module '*.png'; // todo arquivo terminado em png, pode ser lido pelo typescript;
---
+++ src/pages/Landing/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+const syles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#8257E5',
+  }
+});
+
+export default styles;
