bac929d59775b31726efcff5dc3862204078a206
André <andre.murillo.correa@gmail.com>
2020-08-09 18:02:34 -0300

    create study page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -17,6 +17,10 @@ function Landing() {
   function handleNavigationToGiveClassesPage() {
     navigate('GiveClasses'); // referencia o atributo "name" do componente Screen
   }
+
+  function handleNavigateToStudyPages() {
+    navigate('Study');
+  }
   
   return (
     <View style={styles.container}>
@@ -28,7 +32,7 @@ function Landing() {
       </Text>
 
       <View style={styles.buttonsContainer}>
-        <RectButton style={[styles.button, styles.buttonPrimary]}>
+        <RectButton onPress={handleNavigateToStudyPages} style={[styles.button, styles.buttonPrimary]}>
           <Image source={studyIcon} />
           
           <Text style={styles.buttonText}>Estudar</Text>
